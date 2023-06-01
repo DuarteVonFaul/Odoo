b014aff319b7350d300685dbcacb5a3825e4e281
DuarteVonFaul <duarteriograndense@gmail.com>
2023-05-31 23:33:05 -0300

    Schema geral Odoo

---
+++ addons/Pedidos/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Pedidos",
+    'version': '1.0',
+    'author': "Guilherme Duarte Sousa",
+    'category': 'sale_management',
+    'summary':'Modulo responsavel pelo gerenciamento de pedidos',
+    'description': """
+    Description text
+    """,
+
+    'data': [],
+    'depends': [],
+}
